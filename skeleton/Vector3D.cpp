2cc01d4a94c49b9e5ab42b5ccee2f0646c1c96ab
Chenlinjia Yi 👑 <yichenlinjia@gmail.com>
2026-09-18 10:20:19 +0200

    V0.0.2
    Se ha creado clase Vector3D con funciones basicas para operar

---
+++ skeleton/Vector3D.cpp
@@ -0,0 +1,53 @@
+#include "Vector3D.h"
+#include <cmath>
+
+float Vector3D::magnitude() const {
+	return std::sqrt(x * x + y * y + z * z);
+}
+
+Vector3D Vector3D::normalize() const {
+	float mag = magnitude();
+
+	// evitar division por cero
+	if (mag == 0) return Vector3D(0, 0, 0);
+	return Vector3D(x / mag, y / mag, z / mag);
+}
+
+float Vector3D::dot(const Vector3D& v) const {
+	return x * v.x + y * v.y + z * v.z;
+}
+
+Vector3D Vector3D::cross(const Vector3D& v) const {
+	return Vector3D(
+		y * v.z - z * v.y,
+		z * v.x - x * v.z,
+		x * v.y - y * v.x
+	);
+}
+
+Vector3D Vector3D::operator+(const Vector3D& v) const {
+	return Vector3D(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3D Vector3D::operator-(const Vector3D& v) const {
+	return Vector3D(x - v.x, y - v.y, z - v.z);
+}
+
+Vector3D Vector3D::operator*(float escalar) const {
+	return Vector3D(x * escalar, y * escalar, z * escalar);
+}
+
+Vector3D& Vector3D::operator=(const Vector3D& v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+	return *this;
+}
+
+Vector3D& Vector3D::operator+=(const Vector3D& v) {
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	return *this;
+}
+
---
+++ skeleton/Vector3D.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "PxPhysicsAPI.h"
+using namespace physx;
+
+class Vector3D
+{
+public:
+
+	float x, y, z;
+	
+	//constructor por defecto 
+	Vector3D(int x,int y, int z) : x(x = 0), y(y = 0), z(z = 0) {}
+
+	//constructor a traves de un PxVec3 de physx
+	Vector3D(physx::PxVec3 vec3) : x(vec3.x), y(vec3.y), z(vec3.z) {}
+
+	//conversion explicita desde clase Vector3D a clase physx::PxVec3
+	physx::PxVec3 toPxVec3() const { return physx::PxVec3(x, y, z); }
+
+	// devuelve la magnitud del vector
+	float magnitude() const;
+
+	//normaliza el vector y devuelve un nuevo vector normalizado
+	Vector3D normalize() const;
+
+	//productor escalar
+	float dot(const Vector3D& v) const;
+	
+	//producto vectorial
+	Vector3D cross(const Vector3D& v) const;
+
+	//suma de vectores
+	Vector3D operator+(const Vector3D& v) const;
+
+	//resta de vectores
+	Vector3D operator-(const Vector3D& v) const;
+
+	//multiplicacion por escalar
+	Vector3D operator*(float escalar) const;
+
+	//asignar a un vector
+	Vector3D& operator=(const Vector3D& v);
+
+	//suma de vectores y asignacion
+	Vector3D& operator+=(const Vector3D& v);
+};
+
